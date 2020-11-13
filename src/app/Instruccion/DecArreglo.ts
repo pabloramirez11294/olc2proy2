f9745201d4af9dca3109a26072760f15fe509666
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2020-11-13 06:55:43 -0600

    Arreglo declaracion

--- src/app/Instruccion/DecArreglo.ts
+++ src/app/Instruccion/DecArreglo.ts
@@ -3,6 +3,7 @@ import { Instruction } from "../Modelos/Instruction";
 import { Environment, Simbolo } from "../Entornos/Environment";
 import {Type} from "../Modelos/Retorno";
 import {Error_} from '../Reportes/Errores';
+import { Data } from "../Data/Data";
 export class DecArreglo extends Instruction{
     public tamano=false;
     public constante:boolean=false;
@@ -12,18 +13,50 @@ export class DecArreglo extends Instruction{
     }
 
     public execute(amb: Environment) { 
-        //amb.guardarArr(this.id, undefined, this.tipo,this.tipoArreglo, this.dim,this.line,this.column,this.constante);
-        
-   /*     if(valor.type!= 5 && valor.type != this.tipo){
-            throw new Error_(this.line, this.column, 'Semantico',
-            'DECLARACION: no coincide el tipo con el valor, valor:' + valor.value+", tipo: "+this.tipo ,amb.getNombre());
-        }*/
+        const data = Data.getInstance();
+        data.addComentario('DecArreglo Inicia');
+        const sim:Simbolo = amb.guardar(this.id, this.tipo,this.line,this.column,this.constante);
+
         if(this.exp==undefined){
             amb.guardarArr(this.id, undefined, this.tipo,this.tipoArreglo, this.dim,this.line,this.column,this.constante);
+        }else if(this.tamano){
+             //newarrya
+            const size = this.exp.execute(amb);
+            if(size.type != Type.NUMBER){
+                throw new Error_(this.line, this.column, 'Semantico',
+                    'DECLARACION Arreglo: tiene que ser Number el new Array',amb.getNombre());
+            }
+            const tmepSize = data.newTmp();
+            data.addExpression(tmepSize,'h');
+            data.addSetHeap('h',size.value);
+            data.addExpression('h','h',size.value,'+');
+            data.nextHeap();
+            //declaracion
+            if(this.tipoArreglo==Type.NUMBER){           
+                const newTemp = data.newTmp();
+                const label=data.newLabel() ,label2=data.newLabel();
+                data.addExpression(newTemp,tmepSize,'1','+')
+                data.addLabel(label)
+                data.addIf(newTemp,'h','==',label2);
+                if(this.dim>0){
+                    if(this.tipoArreglo ==Type.NUMBER){
+                        data.addSetHeap(newTemp,'0');
+                    }
+                }else{
+                    data.addSetHeap(newTemp,'-1');
+                }
+                data.addExpression(newTemp,newTemp,'1','+');
+                data.addGoto(label);
+                data.addLabel(label2);
+                data.addSetStack(sim.valor,tmepSize);
+                sim.tipoArreglo=this.tipo;
+                sim.dim=this.dim;
+            }
+
         }else{
             const valor = this.exp.execute(amb);
             amb.guardarArr(this.id, valor.value, this.tipo,this.tipoArreglo, this.dim,this.line,this.column,this.constante);    
         }
-       
+        data.addComentario('DecArreglo Termina');
     }
 }
